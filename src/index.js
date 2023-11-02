7217aee7001bec515be4de2d3b2d12b6dbeb54a2
ji3ba <lyovkin_artyom@mail.ru>
2023-11-02 20:49:46 +0300

    open modal window functional was added

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@ const sum = (a,b) => a + b
 console.log(sum(2,3))
 console.log(sum(5,3))
 console.log(sum(1,3))
+
+document.querySelector('.modal__button').addEventListener('click', () => {
+    document.querySelector('.modal').classList.add('modal--active')
+    document.querySelector('.modal__container').classList.add('modal__container--active')
+})
