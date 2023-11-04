b2c24525e88058d9b16c3cd1d5054b4d1612839a
JI3BA <lyovkin_artyom@mail.ru>
2023-11-04 20:59:37 +0300

    main index file was changed, render function was added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import './index.html'
 import './style.sass'
 import './maskPhone'
+import {closeModalWindow, openModalWindow} from "./modal";
 
-const sum = (a,b) => a + b
-
-console.log(sum(2,3))
-console.log(sum(5,3))
-console.log(sum(1,3))
-
-document.querySelector('.modal__button').addEventListener('click', () => {
-    document.querySelector('.modal').classList.add('modal--active')
-    document.querySelector('.modal__container').classList.add('modal__container--active')
-    document.querySelector('body').classList.add('modal__open')
-})
+(function render(){
+    openModalWindow()
+    closeModalWindow()
+}())
