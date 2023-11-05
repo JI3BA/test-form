fec562cde77499decf154b7a8ca47cc9d45268aa
JI3BA <lyovkin_artyom@mail.ru>
2023-11-05 20:05:31 +0300

    modal js file was fixed

--- src/modal.js
+++ src/modal.js
@@ -5,14 +5,14 @@ export const openModalWindow = () => {
     document.querySelector('.modal__button').addEventListener('click', () => {
         document.querySelector('.modal').classList.add('modal--active')
         document.querySelector('.modal__container').classList.add('modal__container--active')
-        document.querySelector('body').classList.add('modal__open')
+        document.querySelector('body').classList.add('lock')
     })
 }
 
 export const closeModalWindow = () => {
-    document.querySelector('.modal__close').addEventListener('click', () => {
         document.querySelector('.modal').classList.remove('modal--active')
         document.querySelector('.modal__container').classList.remove('modal__container--active')
-        document.querySelector('body').classList.remove('modal__open')
-    })
+        document.querySelector('body').classList.remove('lock')
 }
+
+document.querySelector('.modal__close').addEventListener('click', closeModalWindow)
