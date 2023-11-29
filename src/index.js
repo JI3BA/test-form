28d1d9cf9d9dfadf3ea820180e733bbdfd5da882
ji3ba <lyovkin_artyom@mail.ru>
2023-11-29 19:56:34 +0300

    AjaxRequest file was imported to main (index) js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './index.html'
 import './style.sass'
 import './maskPhone'
 import './validation'
+import './AjaxRequest'
 import {closeModalWindow, openModalWindow} from "./modal";
 
 (function render(){
