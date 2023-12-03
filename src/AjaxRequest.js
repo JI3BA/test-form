f139c3597d59e43443a633dca218e8a5e02a0c9c
JI3BA <lyovkin_artyom@mail.ru>
2023-12-03 20:52:02 +0300

    ajax request was changed (XMLHttpRequest was changed to fetch)

--- src/AjaxRequest.js
+++ src/AjaxRequest.js
@@ -1,30 +1,24 @@
-
-
-export  const AjaxRequest = (name,email,phone,message) => {
-    const formData = {
-        name: name,
-        email: email,
-        phone: phone,
-        message: message
-    };
-
-    const xhr = new XMLHttpRequest();
-
-    xhr.open('POST', '/data', true)
-
-    xhr.setRequestHeader('Content-Type', 'application/json');
-
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            console.log(xhr.responseText);
-        }
-    };
-
-    xhr.onerror = function() {
-        console.error('Ошибка при выполнении запроса.');
-    };
-
-    const jsonData = JSON.stringify(formData);
-
-    xhr.send(jsonData);
+export function postData(url, data) {
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Ошибка при выполнении запроса.');
+            }
+        })
+        .then(data => {
+            console.log('Успешный ответ сервера:', data);
+            return data;
+        })
+        .catch(error => {
+            console.error('Ошибка при выполнении запроса:', error);
+            throw error;
+        });
 }
