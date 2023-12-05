c9e59cf5d984002a8bf72d09e53b175b8fc54a7d
JI3BA <lyovkin_artyom@mail.ru>
2023-12-05 17:59:46 +0300

    Trying to start the server without errors.

--- src/ajaxServer.js
+++ src/ajaxServer.js
@@ -1,23 +1,50 @@
+//const express = require('express');
+// const fs = require('fs');
+// const app = express();
+
+// app.use(express.json());
+//
+// // Маршрут для сохранения данных
+// app.post('/api/save-data', (req, res) => {
+//     const jsonData = JSON.stringify(req.body);
+//
+//     fs.writeFile('data.json', jsonData, (err) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send('Произошла ошибка при сохранении данных.');
+//         } else {
+//             res.send('Данные успешно сохранены.');
+//         }
+//     });
+// });
+
+// app.listen(3000, () => {
+//     console.log('Сервер запущен на порту 3000');
+// });
+
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Маршрут для сохранения данных
 app.post('/api/save-data', (req, res) => {
-    const jsonData = JSON.stringify(req.body);
+    const data = req.body;
+    const jsonData = JSON.stringify(data, null, 2);
 
-    fs.writeFile('data.json', jsonData, (err) => {
+    fs.writeFile(path.join(__dirname, 'data.json'), jsonData, (err) => {
         if (err) {
             console.error(err);
-            res.status(500).send('Произошла ошибка при сохранении данных.');
+            res.status(500).send('An error occurred while saving the data.');
         } else {
-            res.send('Данные успешно сохранены.');
+            res.send('Data successfully saved.');
         }
     });
 });
 
-app.listen(3000, () => {
-    console.log('Сервер запущен на порту 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
