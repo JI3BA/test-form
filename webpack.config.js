99d0739f30ee223d0875a70b99f13efb67e5f8d4
JI3BA <lyovkin_artyom@mail.ru>
2023-12-05 17:58:42 +0300

    webpack config was changed (different plugins for express server were installed)

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,50 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
     mode: 'development',
+    devtool: 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.js'),
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src', 'index.html'),
             title: 'Development',
         }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './src/assets',
+                    to: 'assets',
+                },
+            ],
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new WebpackManifestPlugin(),
     ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+        clean: true,
+        publicPath: '/',
     },
     devServer: {
         watchFiles: path.join(__dirname, 'src'),
         port: 3000,
+        // proxy: {
+        //     '/api': {
+        //         target: 'http://localhost:3000/',
+        //         secure: false
+        //     }
+        // }
+        hot: true,
+        proxy: {
+            '/api': 'http://localhost:3000',
+        },
     },
     module: {
         rules: [
@@ -32,6 +59,16 @@ module.exports = {
                     "css-loader",
                     "sass-loader",
                 ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
             },
         ],
     },
